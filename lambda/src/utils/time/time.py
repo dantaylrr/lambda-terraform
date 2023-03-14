70b7c78f19369e32bf2d0cffc132b2523c294133
Daniel Taylor <dannyyyyt@gmail.com>
2023-03-14 21:15:44 +0000

    Adding some unit testing capability for actions testing. (#1)

--- lambda/src/utils/time/time.py
+++ lambda/src/utils/time/time.py
@@ -9,9 +9,16 @@ class Time(object):
 
         super().__init__(**kwargs)
 
-    def get_the_current_time(self):
-        # Get current timestamp.
-        now = datetime.now()
-        # Return year, month, day, hour.
+    def get_the_current_time(self, custom_datetime):
+        # custom_datetime will never be specified here & is only used for testing purposes.
+        if not custom_datetime:
+            # Get current timestamp.
+            now = datetime.now()
+            # Return year, month, day, hour.
+            year, month, day, hour, minute = now.year, now.month, now.day, now.hour, now.minute
 
-        return now.year, now.month, now.day, now.hour, now.minute
+        # Won't be used, only for unit testing purposes.
+        else:
+            year, month, day, hour, minute = custom_datetime.year, custom_datetime.month, custom_datetime.day, custom_datetime.hour, custom_datetime.minute
+        
+        return year, month, day, hour, minute
