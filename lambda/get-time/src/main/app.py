9e89ff6e1b3535c5d8562ddc8d1adba40f142360
dantaylrr <dannyyyyt@gmail.com>
2023-03-20 14:45:20 +0000

    Correcting syntax.

--- lambda/get-time/src/main/app.py
+++ lambda/get-time/src/main/app.py
@@ -8,6 +8,6 @@ def lambda_handler(event, context):
 
     # Return the time from lambda.
     return {
-        'statusCode': 200
+        'statusCode': 200,
         'body': json.dumps(f"Hello from Lambda! The current year is {year}, the current month is {month}, the current day is {day}, the current hour is {hour} & the current minute is {minute}.")
         }
